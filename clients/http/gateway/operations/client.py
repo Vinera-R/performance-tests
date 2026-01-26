06c5970b3a4b6273275a704c1aee6a32d04f5cb6
Vinera <vinera.rakhmatullina@gmail.com>
2026-01-26 15:30:53 +0300

    Практикуемся в использовании HTTP API клиентов. Практика использования HTTP API-клиентов: создание операции пополнения счета

--- clients/http/gateway/operations/client.py
+++ clients/http/gateway/operations/client.py
@@ -3,21 +3,71 @@ from clients.http.client import HTTPClient
 from httpx import Response, QueryParams
 from typing import TypedDict
 
-class GetOperationResponse(TypedDict):
+from clients.http.gateway.client import build_gateway_http_client
+
+
+class OperationDict(TypedDict):
+    id: str
+    type: str
+    status: str
+    amount: float
+    cardId: str
+    category: str
+    createdAt: str
+    accountId: str
+
+class GetOperationResponseDict(TypedDict):
     """Тип данных для ответа по конкретной операции."""
-    operation_id: str
+    operation_id: OperationDict
+
+
+
+class OperationReceiptDict(TypedDict):
+        url: str
+        document: str
 
 class GetOperationReceiptResponse(TypedDict):
     """Тип данных для ответа с чеком операции."""
-    operation_id: str
+    operation_id: OperationReceiptDict
+
+
+
+class OperationDict(TypedDict):
+    id: str
+    type: str
+    status: str
+    amount: float
+    cardId: str
+    category: str
+    createdAt: str
+    accountId: str
+
+class GetOperationsResponseDict(TypedDict):
+    operations: list[OperationDict]
 
 class GetOperationsQueryDict(TypedDict):
     """Тип данных для параметров запроса списка операций."""
     accountId: str
 
+
+class OperationsSummaryDict(TypedDict):
+    spentAmount: float
+    receivedAmount: float
+    cashbackAmount: float
+
+class GetOperationsSummaryResponseDict(TypedDict):
+    summary: OperationsSummaryDict
+
 class GetOperationSummaryQueryDict(TypedDict):
     """Тип данных для параметров запроса статистики операций."""
     accountId: str
+
+
+
+
+class MakeFeeOperationResponseDict(TypedDict):
+    operation: list[OperationDict]
+
 
 class MakeFeeOperationRequestDict(TypedDict):
     """Тип данных для запроса создания операции комиссии."""
@@ -26,12 +76,22 @@ class MakeFeeOperationRequestDict(TypedDict):
     cardId: str
     accountId: str
 
+
+
+class MakeTopUpOperationResponseDict(TypedDict):
+    operation: list[OperationDict]
+
+
 class MakeTopUpOperationRequestDict(TypedDict):
     """Тип данных для запроса создания операции пополнения."""
     status: str
     amount: float
     cardId: str
     accountId: str
+
+
+class MakeCashbackOperationResponseDict(TypedDict):
+    operation: list[OperationDict]
 
 class MakeCashbackOperationRequestDict(TypedDict):
     """Тип данных для запроса создания операции кэшбэка."""
@@ -40,12 +100,20 @@ class MakeCashbackOperationRequestDict(TypedDict):
     cardId: str
     accountId: str
 
+
+class MakeTransferOperationResponseDict(TypedDict):
+    operation: list[OperationDict]
+
 class MakeTransferOperationRequestDict(TypedDict):
     """Тип данных для запроса создания операции перевода."""
     status: str
     amount: float
     cardId: str
     accountId: str
+
+
+class MakePurchaseOperationResponseDict(TypedDict):
+    operation: list[OperationDict]
 
 class MakePurchaseOperationRequestDict(TypedDict):
     """Тип данных для запроса создания операции покупки."""
@@ -55,12 +123,19 @@ class MakePurchaseOperationRequestDict(TypedDict):
     accountId: str
     category: str
 
+
+class MakeBillPaymentOperationResponseDict(TypedDict):
+    operation: list[OperationDict]
+
 class MakeBillPaymentOperationRequestDict(TypedDict):
     """Тип данных для запроса создания операции оплаты по счету."""
     status: str
     amount: float
     cardId: str
     accountId: str
+
+class MakeCashWithdrawalOperationResponseDict(TypedDict):
+    operation: list[OperationDict]
 
 class MakeCashWithdrawalOperationRequestDict(TypedDict):
     """Тип данных для запроса создания операции снятия наличных."""
@@ -173,6 +248,108 @@ class OperationsGatewayHTTPClient(HTTPClient):
         :return: Объект Response с результатом выполнения.
         """
         return self.post("/api/v1/operations/make-cash-withdrawal-operation", json=request)
+
+    def get_operation(self, operation_id: str) -> GetOperationResponseDict:
+        response = self.get(f"/api/v1/operations/{operation_id}")
+        return response.json()
+
+    def get_operation_receipt(self, operation_id: str) -> GetOperationReceiptResponse:
+        response = self.get(f"/api/v1/operations/{operation_id}/receipt")
+        return response.json()
+
+    def get_operations(self, account_id: str) -> GetOperationsResponseDict:
+        query = GetOperationsQueryDict(accountId=account_id)
+        response = self.get_operations_api(query)
+        return response.json()
+
+    def get_operations_summary(self, account_id: str) -> GetOperationsSummaryResponseDict:
+        query = GetOperationSummaryQueryDict(accountId=account_id)
+        response = self.get(f"/api/v1/operations/summary")
+        return response.json()
+
+    def make_fee_operation(self, card_id: str, account_id: str) -> MakeFeeOperationResponseDict:
+        request = MakeFeeOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_fee_operation_api(request)
+        return response.json()
+
+    def make_top_up_operation(self, card_id: str, account_id: str) -> MakeTopUpOperationResponseDict:
+        request = MakeTopUpOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_top_up_operation_api(request)
+        return response.json()
+
+    def make_cashback_operation(self, card_id: str, account_id: str) -> MakeCashbackOperationResponseDict:
+        request = MakeCashbackOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_cashback_operation_api(request)
+        return response.json()
+
+    def make_transfer_operation(self, card_id: str, account_id: str) -> MakeTransferOperationResponseDict:
+        request = MakeTransferOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_transfer_operation_api(request)
+        return response.json()
+
+    def make_purchase_operation(self, card_id: str, account_id: str) -> MakePurchaseOperationResponseDict:
+        request = MakePurchaseOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id,
+            category="str"
+
+        )
+        response = self.make_purchase_operation_api(request)
+        return response.json()
+
+    def make_bill_payment_operation(self, card_id: str, account_id: str) -> MakeBillPaymentOperationResponseDict:
+        request = MakeBillPaymentOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_bill_payment_operation_api(request)
+        return response.json()
+
+    def make_cash_withdrawal_operation(self, card_id: str, account_id: str) -> MakeCashWithdrawalOperationResponseDict:
+        request = MakeCashWithdrawalOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_cash_withdrawal_operation_api(request)
+        return response.json()
+
+
+
+# Добавляем builder для DocumentsGatewayHTTPClient
+def build_operations_gateway_http_client() -> OperationsGatewayHTTPClient:
+    """
+           Функция создаёт экземпляр OperationsGatewayHTTPClient с уже настроенным HTTP-клиентом.
+
+           :return: Готовый к использованию OperationsGatewayHTTPClient.
+           """
+    return OperationsGatewayHTTPClient(client=build_gateway_http_client())
+
 
 
 
