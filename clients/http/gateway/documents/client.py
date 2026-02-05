550a5e7818e61e4704b76b730ef2894b847eeb76
Vinera <vinera.rakhmatullina@gmail.com>
2026-02-05 15:52:28 +0300

    Применяем Pydantic в HTTP API клиентах. Практикуемся в применении Pydantic в API клиентах: документы

--- clients/http/gateway/documents/client.py
+++ clients/http/gateway/documents/client.py
@@ -1,19 +1,11 @@
 from httpx import Response
-from typing_extensions import TypedDict
 
 from clients.http.client import HTTPClient
 from clients.http.gateway.client import build_gateway_http_client
-
-
-class DocumentDict(TypedDict):
-        url: str
-        document: str
-
-class GetTariffDocumentResponseDict(TypedDict):
-    tariff: DocumentDict
-
-class GetContractDocumentResponseDict(TypedDict):
-    contract: DocumentDict
+from clients.http.gateway.documents.schema import (
+GetTariffDocumentResponseSchema,
+GetContractDocumentResponseSchema,
+)
 
 class DocumentsGatewayHTTPClient(HTTPClient):
     """
@@ -38,7 +30,7 @@ class DocumentsGatewayHTTPClient(HTTPClient):
         """
         return self.get(f"/api/v1/documents/contract-document/{account_id}")
 
-    def get_tariff_document(self, account_id: str) -> GetTariffDocumentResponseDict:
+    def get_tariff_document(self, account_id: str) -> GetTariffDocumentResponseSchema:
         response = self.get_tariff_document_api(account_id)
         print(f"Response status: {response.status_code}")
         print(f"Response content: {response.text}")
@@ -47,10 +39,10 @@ class DocumentsGatewayHTTPClient(HTTPClient):
             raise Exception(f"Ошибка при получении тарифа: статус {response.status_code}")
         if not response.content:
             raise Exception("Пустой ответ от сервера при получении тарифа")
-        return response.json()
+        return GetTariffDocumentResponseSchema.model_validate_json(response.text)
 
 
-    def get_contract_document(self, account_id: str) -> GetContractDocumentResponseDict:
+    def get_contract_document(self, account_id: str) -> GetContractDocumentResponseSchema:
         response = self.get_contract_document_api(account_id)
         print(f"Response status: {response.status_code}")
         print(f"Response content: {response.text}")
@@ -59,7 +51,7 @@ class DocumentsGatewayHTTPClient(HTTPClient):
             raise Exception(f"Ошибка при получении контракта: статус {response.status_code}")
         if not response.content:
             raise Exception("Пустой ответ от сервера при получении контракта")
-        return response.json()
+        return GetContractDocumentResponseSchema.model_validate_json(response.text)
 
 
 # Добавляем builder для DocumentsGatewayHTTPClient
