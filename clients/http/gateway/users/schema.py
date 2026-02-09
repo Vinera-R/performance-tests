76828858ae2282343c8e0e9382dc67543e180876
Vinera <vinera.rakhmatullina@gmail.com>
2026-02-09 07:13:36 +0300

    Знакомство с Faker

--- clients/http/gateway/users/schema.py
+++ clients/http/gateway/users/schema.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel, Field, ConfigDict, EmailStr
-
+from tools.fakers import fake
 
 
 # Добавили описание структуры пользователя
 class UserSchema(BaseModel):
+
     """
     Описание структуры пользователя.
     """
@@ -30,11 +31,16 @@ class CreateUserRequestSchema(BaseModel):
     """
     Структура данных для создания нового пользователя.
     """
-    email: EmailStr
-    last_name: str = Field(alias='lastName')
-    first_name: str = Field(alias='firstName')
-    middle_name: str = Field(alias='middleName')
-    phone_number: str = Field(alias='phoneNumber')
+    # Добавили генерацию случайного email
+    email: EmailStr = Field(default_factory=fake.email)
+    # Добавили генерацию случайной фамилии
+    last_name: str = Field(alias="lastName", default_factory=fake.last_name)
+    # Добавили генерацию случайного имени
+    first_name: str = Field(alias="firstName", default_factory=fake.first_name)
+    # Добавили генерацию случайного отчества
+    middle_name: str = Field(alias="middleName", default_factory=fake.middle_name)
+    # Добавили генерацию случайного номер телефона
+    phone_number: str = Field(alias="phoneNumber", default_factory=fake.phone_number)
 
 
 # Добавили описание структуры ответа создания пользователя
@@ -44,3 +50,4 @@ class CreateUserResponseSchema(BaseModel):
     """
     user: UserSchema
 
+print(CreateUserRequestSchema())
